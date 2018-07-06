694dd5c181891accf58c374deb6d22c835f9c29b
katy@karstaway.com <katy@karstaway.com>
2018-07-06 00:34:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+var katzDeli = [];
+
+function takeANumber (katzDeli, name) {
+  katzDeli.push (name)
+  return ("Welcome, " + name + ". You are number " + (katzDeli.length) + " in line.")
+}
+
+var deliLine =[]
+function nowServing (katzDeli, deliLine) {
+  
+ if (katzDeli.length > 1 ){
+   
+ return ("Currently serving " + katzDeli[0] + ".");
+katzDeli.shift ();
+
+ }
+ else {
+   return ("There is nobody waiting to be served!")
+}
+}
+
+function currentLine (line) {
+  
+  if ( line.length === 0) {
+  return ("The line is currently empty.")
+  }
+  
+  var i = 0
+  i++;
+ 
+   do {
+    line.push ("The line is currently: " + [i] +". " + line[i] + ",") 
+   }
+   while (i < line.length +1 )
+   return line
+}
